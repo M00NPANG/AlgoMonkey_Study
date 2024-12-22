120d674a0ee8be7776e20af8b8f73118e21116f7
asungkim <asungkim007@naver.com>
2024-12-22 14:04:08 +0900

    4단계 8까지 진행 완료

--- level4/_3052.java
+++ level4/_3052.java
@@ -1,6 +1,10 @@
-import java.util.*;
+package level4;
 
-public class Main {
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class _3052 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
 
