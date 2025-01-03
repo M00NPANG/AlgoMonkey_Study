014d26bd2dfefac6deeafc70d98983e5d4561ee1
asungkim <asungkim007@naver.com>
2025-01-03 10:42:54 +0900

    recover

--- asungkim_java/src/week1/level1/_10171.java
+++
@@ -1,9 +0,0 @@
-package week1.level1;
-public class _10171 {
-    public static void main(String[] args) {
-        System.out.println("\\    /\\");
-        System.out.println(" )  ( ')");
-        System.out.println("(  /  )");
-        System.out.println("\\(__)|");
-    }
-}
