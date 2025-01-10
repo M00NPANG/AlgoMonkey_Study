c649cddf694d2041b5b4872c2e0ccae1d829d946
asungkim <asungkim007@naver.com>
2025-01-10 16:13:22 +0900

    baekjoon lv10 done

---
+++ asungkim_java/src/baekjoon/level10/_14215.java
@@ -0,0 +1,24 @@
+package baekjoon.level10;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class _14215 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] arr = new int[3];
+        for (int i = 0; i < 3; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        Arrays.sort(arr);
+        if (arr[2] >= arr[0] + arr[1]) {
+            arr[2] = arr[0] + arr[1] - 1;
+        }
+
+        System.out.println(arr[0]+arr[1]+arr[2]);
+
+
+    }
+}
