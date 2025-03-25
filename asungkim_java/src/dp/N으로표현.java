b78f1aea6bf2ab6b3c9ff9317f26789b35a72e52
asungkim <asungkim007@naver.com>
2025-03-25 19:49:21 +0900

    DP 3문제 완료

--- asungkim_java/src/dp/N으로표현.java
+++ asungkim_java/src/dp/N으로표현.java
@@ -12,35 +12,36 @@ public class N으로표현 {
      * n이 작은 수인 것을 보고 dfs로 풀기로 생각
      */
     class Solution1 {
-        static int answer=Integer.MAX_VALUE;
+        static int answer = Integer.MAX_VALUE;
+
         public int solution(int N, int number) {
 
-            dfs(N,number,0,0);
+            dfs(N, number, 0, 0);
 
-            if (answer>8) return -1;
+            if (answer > 8) return -1;
 
             return answer;
         }
 
-        public void dfs(int N,int number,int start,int cnt) {
+        public void dfs(int N, int number, int start, int cnt) {
             // 8번보다 크면 패스
-            if (cnt>8) {
+            if (cnt > 8) {
                 return;
             }
 
             // 숫자가 같아지면 최솟값인지 체크
-            if (start==number) {
-                answer=Math.min(answer,cnt);
+            if (start == number) {
+                answer = Math.min(answer, cnt);
                 return;
             }
 
-            int tmp=0;
-            for (int i=1;i<=8;i++) {
-                tmp=tmp*10+N;
-                dfs(N,number,start+tmp,cnt+i);
-                dfs(N,number,start-tmp,cnt+i);
-                dfs(N,number,start*tmp,cnt+i);
-                dfs(N,number,start/tmp,cnt+i);
+            int tmp = 0;
+            for (int i = 1; i <= 8; i++) {
+                tmp = tmp * 10 + N;
+                dfs(N, number, start + tmp, cnt + i);
+                dfs(N, number, start - tmp, cnt + i);
+                dfs(N, number, start * tmp, cnt + i);
+                dfs(N, number, start / tmp, cnt + i);
             }
         }
     }
@@ -52,23 +53,23 @@ public class N으로표현 {
      */
     class Solution2 {
         public int solution(int N, int number) {
-            List<Set<Integer>> dp=new ArrayList<>();
-            for (int i=0;i<=8;i++) {
+            List<Set<Integer>> dp = new ArrayList<>();
+            for (int i = 0; i <= 8; i++) {
                 dp.add(new HashSet<>());
             }
 
-            for (int i=1;i<=8;i++) {
-                int num=Integer.parseInt(String.valueOf(N).repeat(i));
+            for (int i = 1; i <= 8; i++) {
+                int num = Integer.parseInt(String.valueOf(N).repeat(i));
                 dp.get(i).add(num);
 
-                for (int j=1;j<i;j++) {
+                for (int j = 1; j < i; j++) {
                     // N=5 , i=5 => 55555 -> (5,5555) , (55,555) ...
                     for (int a : dp.get(j)) {
-                        for (int b : dp.get(i-j)) {
-                            dp.get(i).add(a+b);
-                            dp.get(i).add(a-b);
-                            dp.get(i).add(a*b);
-                            if (b!=0) dp.get(i).add(a/b);
+                        for (int b : dp.get(i - j)) {
+                            dp.get(i).add(a + b);
+                            dp.get(i).add(a - b);
+                            dp.get(i).add(a * b);
+                            if (b != 0) dp.get(i).add(a / b);
                         }
                     }
                 }
